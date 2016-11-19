61d6be1c018e4d73c90aa73c933a7f063f84df3c
Jim Baker <jam.baker1217@gmail.com>
2016-11-19 09:12:59 -0600

    update to signup and login

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -3,8 +3,8 @@ package org.launchcode.blogz.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.hibernate.Session;
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,26 +28,38 @@ public class AuthenticationController extends AbstractController {
 		String verify = request.getParameter("verify");
 		
 		// validate parameters (username, password, verify)
+		boolean has_error = false;
 		if (!User.isValidUsername(username)) {
-			throw new IllegalArgumentException("Invalid username");
-		}
-		if (!User.isValidPassword(password)) {
-			throw new IllegalArgumentException("Invalid passowrd");
-		}
-		if (password != verify) {
-			throw new IllegalArgumentException("Passwords do not match");
+			has_error = true;
+			model.addAttribute("username_error", "Invalid Username");
+		} else {
+			model.addAttribute("username", username);
 		}
 		
+		if (!User.isValidPassword(password)) {
+			has_error = true;
+			model.addAttribute("password_error", "Invalid password");
+		}
+		
+		if (!password.equals(verify)) {
+			has_error = true;
+			model.addAttribute("verify_error", "Passwords do not match");
+		} 
+
 		// if they validate, create a new user and store in the session
+		if (has_error == true) {
+			return "signup";
+		}
+		
 		else {
 		User user = new User(username, password);
 		
-		HttpSession thisSession = request.getSession();
+		HttpSession thisSession = request.getSession();	
 		
 		setUserInSession(thisSession, user);
-		}
 		
 		return "redirect:blog/newpost";
+		}
 		
 		//Session thisSession = request.getSession(); (code that gets current session)
 	}
@@ -62,11 +74,16 @@ public class AuthenticationController extends AbstractController {
 		
 		// TODO - implement login
 		// get parameters from request
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
 		// get user by their username
+		// User user = UserDao.findByUid(user.getUid());
 		
 		// check that password is correct...
-		
+//		if(!User.isMatchingPassword(password)){
+//
+//		}
 		// if so, then log them in (i.e. setting the user in the session)
 		
 		// Session thisSession = request.getSession(); (code that gets current session)
